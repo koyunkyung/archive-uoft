27a95190d3eaa856de24c5b72d193bbab8522086
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-10-13 00:05:48 -0400

    initial framework & download raw data

---
+++ scripts/02-download_data.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from the FiveThirtyEight's 2024 US Presidential Election Polls (National)
+# Author: Yunkyung Ko
+# Date: 12 October 2024
+# Contact: yunkyung.ko@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: None
+# Any other information needed? None
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Download data ####
+raw_elections_data <-read_csv(file ="https://projects.fivethirtyeight.com/polls/data/president_polls.csv")
+
+#### Save data ####
+write_csv(raw_elections_data, "data/01-raw_data/raw_elections_data.csv")
+
+         
