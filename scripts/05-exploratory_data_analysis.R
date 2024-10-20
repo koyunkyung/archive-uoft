8632784e9066232e5a6d229582d50cc39e03661c
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-10-20 12:33:55 -0400

    model design for model_date, model_date_pollster

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Yunkyung Ko
+# Date: 20 October 2024
+# Contact: yunkyung.ko@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -13,19 +13,9 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+harris_data <- read_csv("data/02-analysis_data/harris_elections_data.csv")
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
 
 
 #### Save model ####
