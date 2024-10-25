1ef00c43488a0b5d840b24fff70579820b92b1e8
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-10-25 16:11:08 -0400

    update 'correlation between predictor variables'

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -52,7 +52,7 @@ harris_data <-
     num_harris = round((pct / 100) * sample_size, 0) # Need number not percent for some models
   ) |>
   group_by(pollster) |>
-  filter(n() > 30) |> # Filter for pollsters with more than 35 polls
+  filter(n() > 20) |> # Filter for pollsters with more than 20 polls
   ungroup()  |>
   filter(pct >= 0 & pct <= 100)
 
